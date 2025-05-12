45c1c66b4b5592d093f568f344f877b8fd9adca6
Bella Rose Wilson <bwilson9893@gmail.com>
2025-05-12 17:35:21 +0000

    appliccation controller and base

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,38 @@
+class GameController < ApplicationController
+  def rules
+  end
+
+  def rock
+    play("rock")
+  end
+
+  def paper
+    play("paper")
+  end
+
+  def scissors
+    play("scissors")
+  end
+
+  private
+
+  def play(player_move)
+    choices = %w[rock paper scissors]
+    computer_move = choices.sample
+
+    outcome = if player_move == computer_move
+                "It's a tie!"
+              elsif (player_move == "rock" && computer_move == "scissors") ||
+                    (player_move == "paper" && computer_move == "rock") ||
+                    (player_move == "scissors" && computer_move == "paper")
+                "You win!"
+              else
+                "You lose!"
+              end
+
+    @computer_move = computer_move
+    @outcome = outcome
+
+    render player_move
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root "game#rules"
 
+  get "/rock", to: "game#rock"
+  get "/paper", to: "game#paper"
+  get "/scissors", to: "game#scissors"
 end
